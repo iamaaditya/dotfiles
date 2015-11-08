d6c852091cec9f99855e8f9c666a195d9170b026
aaditya prakash <aprakash@brandeis.edu>
2015-11-08 00:24:10 -0500

    completed changes for caffe and theano

--- install.sh
+++ install.sh
@@ -3,10 +3,11 @@
 mv ~/.vim ~/.vim.bk
 mv ~/.vimrc ~/.vimrc.bk
 mv ~/.tmux.conf ~/.tmux.conf.bk
-mv ~/.screenrc ~/.screenrc.bk
+# mv ~/.screenrc ~/.screenrc.bk
 
 ln -s ~/dotfiles/vim ~/.vim
 ln -s ~/dotfiles/vim/vimrc ~/.vimrc
 ln -s ~/dotfiles/tmux/.tmux.conf ~/
-ln -s .screenrc ~/
-ln -s ~/dotfiles/vim/ftplugin ~/ftplugin
+ln -s ~/dotfiles/theanorc ~/.theanorc
+# ln -s .screenrc ~/
+# ln -s ~/dotfiles/vim/ftplugin ~/ftplugin
